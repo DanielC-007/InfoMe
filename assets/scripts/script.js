d9b8bb0c6e5a709dcd2c4b3e13e273059b63c0e8
MrMeme777 <brz.danielcarlos@gmail.com>
2025-07-24 17:47:23 -0300

    simples = Melhor

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,27 +4,27 @@ function off() {
   alert("Esta função está temporariamente desativada");
 }
 
-function OnOffPart() {
-  var x = document.getElementById("x");
-  var embed = document.getElementById("divPart");
-  // var body = document.body;
+// function OnOffPart() {
+//   var x = document.getElementById("x");
+//   var embed = document.getElementById("divPart");
+//   // var body = document.body;
 
-  if (x.style.visibility == "hidden" || x.style.visibility == "") {
-    x.style.visibility = "visible";
-    embed.style.visibility = "visible";
-    embed.style.animation = "donwDiv 1s";
-    embed.style.transform = "TranslateY(0px)";
-    // body.style.overflow = "hidden";
-  } else {
-    x.style.visibility = "hidden";
-    embed.style.animation = "upDiv 1s";
-    embed.style.transform = "TranslateY(-200%)";
-    setTimeout(function delay() {
-      embed.style.visibility = "hidden";
-    }, 500);
-    // body.style.overflow = "visible";
-  }
-}
+//   if (x.style.visibility == "hidden" || x.style.visibility == "") {
+//     x.style.visibility = "visible";
+//     embed.style.visibility = "visible";
+//     embed.style.animation = "donwDiv 1s";
+//     embed.style.transform = "TranslateY(0px)";
+//     // body.style.overflow = "hidden";
+//   } else {
+//     x.style.visibility = "hidden";
+//     embed.style.animation = "upDiv 1s";
+//     embed.style.transform = "TranslateY(-200%)";
+//     setTimeout(function delay() {
+//       embed.style.visibility = "hidden";
+//     }, 500);
+//     // body.style.overflow = "visible";
+//   }
+// }
 
 if (!localStorage.getItem("theme")) {
   localStorage.setItem("theme", "light");
